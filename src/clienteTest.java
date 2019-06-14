0d2b3d2f289be7fb5a2c6123513218b3c1008826
Joao Pedro <gandarela98@gmail.com>
2019-06-14 01:11:41 -0300

    Testee da classe Cliente

--- src/clienteTest.java
+++ src/clienteTest.java
@@ -46,10 +46,18 @@ class clienteTest {
 	
 	@Test
 	void testSaveClient() {
-		Cliente c = new Cliente("12345678900", "REBECA FONSECA", "1234567890", "21212121211", "Teste@teste.com", 2, 5, 2019,
+		
+		Random rand = new Random();
+		Cliente c = new Cliente("12345678914", "MEGA REBECA FONSECA", "1114266790", "21212121211", "MEGAREBECATESTE@teste.com", rand.nextInt(31) + 1, rand.nextInt(12) + 1, rand.nextInt(2000) + 1,
+				"RUA TESTE", "CIDADE SUPER TESTE", 330, "20702890");
+		Cliente cl = new Cliente("12346674920", "MEGA TESTE", "1224593890", "21215187241", "MEGATESTE@teste.com", rand.nextInt(31) + 1, rand.nextInt(12) + 1, rand.nextInt(2000) + 1,
+				"RUA TESTa", "CIDADE SUPER TESTE", 330, "20702890");
+		Cliente cli = new Cliente("12243275410", "MEGA REBECA FONSECA SUPER", "1114547690", "21212113651", "SUPERMEGAREBECA@teste.com", rand.nextInt(31) + 1, rand.nextInt(12) + 1, rand.nextInt(2000) + 1,
 				"RUA TESTE", "CIDADE SUPER TESTE", 330, "20702890");
 		try {
 			Cliente.saveClient(c);
+			Cliente.saveClient(cl);
+			Cliente.saveClient(cli);
 		} catch (SQLException e) {
 			e.printStackTrace();
 			fail("save");
